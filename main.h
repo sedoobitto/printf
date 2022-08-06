16db9501c43eb0e8cdfb30c5d2645a772e85685f
Sedoo Bitto <sedoobitto@gmail.com>
2022-08-06 15:37:54 -0700

    .

--- main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdarg.h>
-int _putchar(char c);
-int _printf(const char *format, ...);
-int character(va_list parameter);
-int string_character(va_list parameter);
-int print_digit(va_list paramter);
-#endif
