f38f8ef64dd7e2f9c4fd91606c20b88dc941b241
Superjaks <106969009+Superjaks@users.noreply.github.com>
2022-08-12 22:14:29 +0100

    Create get_size.c

---
+++ get_size.c
@@ -0,0 +1,32 @@
+#include "main.h"
+void print_buffer(char buffer[], int *buff_ind);
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
+#include "main.h"
+#include "main.h"
+#include "main.h"
+#include "main.h"
+/**
+ * get_size - Calculates the size to cast the argument
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ *
+ * Return: Precision.
+ */
+int get_size(const char *format, int *i)
+{
+        int curr_i = *i + 1;
+        int size = 0;
+        if (format[curr_i] == 'l')
+                size = S_LONG;
+        else if (format[curr_i] == 'h')
+                size = S_SHORT;
+        if (size == 0)
+                *i = curr_i - 1;
+        else
+                *i = curr_i;
+        return (size);
+}
